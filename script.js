18fd4f1ef03879bcbbfcc76c9b2da4efc678df24
omkarrajeshinde123-bit <omkarrajeshinde123@gmail.com>
2026-04-08 19:18:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const products = [
   {
     name: "Black T-Shirt",
     price: "₹499",
-    image: "https://via.placeholder.com/200"
+    image: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab"
   },
   {
     name: "Denim Jacket",
     price: "₹1299",
-    image: "https://via.placeholder.com/200"
+    image: "https://images.unsplash.com/photo-1591047139829-d91aecb6caea"
   },
   {
     name: "Casual Shirt",
     price: "₹799",
-    image: "https://via.placeholder.com/200"
+    image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf"
   },
   {
     name: "Jeans",
     price: "₹999",
-    image: "https://via.placeholder.com/200"
+    image: "https://images.unsplash.com/photo-1583001809873-a128495da465"
   }
 ];
 
